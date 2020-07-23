12aa1290c4f55eab2a5205d56ca2b344742a682a
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-23 10:48:52 +0300

    ОТображение информации в модальном окне

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,8 +1,85 @@
 <template>
   <portal v-if="isShow" to="modal">
     <div class="modal-substrate" @click="toggleModal">
-      <v-row no-gutters class="row">
-        <v-col class="modal" cols="5"></v-col>
+      <v-row no-gutters align="center">
+        <v-col class="modal" cols="5" @click.stop="">
+          <v-row v-if="data.eventLabel === 'pip_details'" align="center">
+            <v-spacer></v-spacer>
+            <v-col v-if="data.productDescriptionProps" cols="9">
+              <h2>{{ data.title }}</h2>
+              <p
+                v-for="(paragraph, index) in data.productDescriptionProps
+                  .paragraphs"
+                :key="index"
+              >
+                {{ paragraph }}
+              </p>
+              <p>
+                <b>{{ data.productDescriptionProps.designerLabel }}: </b>
+                {{ data.productDescriptionProps.designerName }}
+              </p>
+
+              <v-expansion-panels v-if="data.accordionObject" accordion flat>
+                <v-expansion-panel
+                  v-for="(accordionItem, index) in accordion"
+                  :id="accordionItem.id"
+                  :key="index"
+                >
+                  <v-expansion-panel-header>{{
+                    accordionItem.title
+                  }}</v-expansion-panel-header>
+                  <v-expansion-panel-content>
+                    <div
+                      v-for="(blocks, name) in accordionItem.contentProps"
+                      :key="name"
+                    >
+                      <div
+                        v-for="(block, indexBlocks) in blocks"
+                        :key="`${indexBlocks}-blocks`"
+                      >
+                        {{ block }}
+                        <!--Используется в "Материалы и уход"-->
+                        <p
+                          v-for="(material, indexMaterial) in block.materials"
+                          :key="`${indexMaterial}-material`"
+                        >
+                          {{ material.material }}
+                        </p>
+
+                        <!--Используется всеми-->
+                        <p
+                          v-for="(text, indexText) in block.texts"
+                          :key="`${indexText}-text`"
+                        >
+                          {{ text }}
+                        </p>
+
+                        <!--Используется в "Информация об упаковке"-->
+                        <div
+                          v-if="
+                            accordionItem.id === 'product-details-packaging'
+                          "
+                        >
+                          <span class="product-detail-header">{{
+                            block.name
+                          }}</span>
+                          <span>{{ block.typeName }}</span>
+                          <span
+                            v-if="block.articleNumber"
+                            class="product-detail-identifier"
+                            >{{ block.articleNumber.label }}:
+                            {{ block.articleNumber.value }}</span
+                          >
+                        </div>
+                      </div>
+                    </div>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-col>
+            <v-spacer></v-spacer>
+          </v-row>
+        </v-col>
       </v-row>
     </div>
   </portal>
@@ -15,6 +92,17 @@ export default {
   computed: mapState({
     isShow: (state) => state.page.modal.isShow,
     data: (state) => state.page.modal.data,
+    accordion: (state) => {
+      const acc = state.page.modal.data.accordionObject
+      const out = []
+      for (const key in acc) {
+        const item = acc[key]
+        if (item) {
+          out.push(item)
+        }
+      }
+      return out
+    },
   }),
   methods: mapActions({
     toggleModal: 'page/toggleModal',
@@ -28,10 +116,6 @@ export default {
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.15);
-
-  .row {
-    height: 100%;
-  }
 
   .modal {
     position: absolute;
