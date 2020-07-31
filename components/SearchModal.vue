ee80d1561faaa3052e3bc59ddd00fc9980087cc3
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-31 09:38:45 +0300

    Верстка модального окна для поиска

--- components/SearchModal.vue
+++ components/SearchModal.vue
@@ -2,7 +2,24 @@
   <portal v-if="isShow && data.type === 'search'" to="modal">
     <div class="modal-substrate" @click="hideModal">
       <v-row class="modal" no-gutters @click.stop="">
-        <v-text-field></v-text-field>
+        <v-spacer></v-spacer>
+        <v-col cols="11">
+          <v-row>
+            <v-col cols="1">
+              <v-btn class="top-indent" icon @click="hideModal">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col cols="11">
+              <v-text-field
+                clearable
+                placeholder="Что вы ищете?"
+                append-icon="mdi-arrow-right"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-spacer></v-spacer>
       </v-row>
     </div>
   </portal>
@@ -26,5 +43,9 @@ export default {
 .modal {
   min-height: 500px;
   background: #fff;
+
+  .top-indent {
+    margin-top: 16px;
+  }
 }
 </style>
