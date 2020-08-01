0d8b86fcd54bc68fde6c32c0f2e7afcd4c301a5d
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-01 12:20:36 +0300

    SEARCH: отображаем полученные данные в боксе

--- components/SearchModal.vue
+++ components/SearchModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <portal v-if="isShow && data.type === 'search'" to="modal">
+  <portal v-if="isShow && modalData.type === 'search'" to="modal">
     <div class="modal-substrate" @click="hideModal">
       <v-row class="modal" no-gutters @click.stop="">
         <v-spacer></v-spacer>
@@ -21,6 +21,67 @@
               ></v-text-field>
             </v-col>
           </v-row>
+
+          <v-row>
+            <v-col cols="12">
+              <v-list>
+                <v-list-item
+                  v-for="(completion, index) in universal"
+                  :key="index"
+                >
+                  <!-- <Искать> -->
+                  <v-list-item-icon v-if="completion.label === 'Искать'">
+                    <v-icon>mdi-magnify</v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content v-if="completion.label === 'Искать'">
+                    <v-list-item-title
+                      v-text="completion.completion.text"
+                    ></v-list-item-title>
+                  </v-list-item-content>
+                  <!-- /<Искать> -->
+
+                  <!-- <Категория> -->
+                  <v-list-item-icon v-if="completion.label === 'Категория'">
+                    <v-icon>mdi-apps</v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content
+                    v-if="completion.label === 'Категория'"
+                    two-line
+                  >
+                    <v-list-item-title
+                      v-text="completion.category.name"
+                    ></v-list-item-title>
+
+                    <v-list-item-subtitle
+                      v-text="'Категория'"
+                    ></v-list-item-subtitle>
+                  </v-list-item-content>
+                  <!-- </Категория> -->
+
+                  <!-- <Товар> -->
+                  <v-list-item-icon v-if="completion.label === 'Товар'">
+                    <v-icon>mdi-chevron-right</v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content
+                    v-if="completion.label === 'Товар'"
+                    two-line
+                  >
+                    <v-list-item-title
+                      v-text="completion.product.name"
+                    ></v-list-item-title>
+
+                    <v-list-item-subtitle
+                      v-text="completion.product.typeName"
+                    ></v-list-item-subtitle>
+                  </v-list-item-content>
+                  <!-- </Товар> -->
+                </v-list-item>
+              </v-list>
+            </v-col>
+          </v-row>
         </v-col>
         <v-spacer></v-spacer>
       </v-row>
@@ -34,7 +95,16 @@ export default {
   name: 'SearchModal',
   computed: mapState({
     isShow: (state) => state.page.modal.isShow,
-    data: (state) => state.page.modal.data,
+    modalData: (state) => state.page.modal.data,
+    universal: (state) =>
+      state.search.data.universal
+        ? state.search.data.universal.filter(
+            (item) =>
+              item.label === 'Искать' ||
+              item.label === 'Товар' ||
+              item.label === 'Категория'
+          )
+        : [],
   }),
   methods: mapActions({
     hideModal: 'page/hideModal',
--- store/search.js
+++ store/search.js
@@ -1,11 +1,16 @@
 import ApiService from '@/services/ApiService.js'
 export const state = () => ({
   value: '',
+  data: {},
 })
 
 export const mutations = {
   SET_VALUE(state, payload) {
     state.value = payload
+  },
+
+  SET_DATA(state, payload) {
+    state.data = payload
   },
 }
 export const actions = {
@@ -14,6 +19,7 @@ export const actions = {
 
     ApiService.getSearch(payload).then((response) => {
       console.log('Response Data', response.data)
+      commit('SET_DATA', response.data.searchBox)
     })
   },
 }
