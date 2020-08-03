25b701a78826f6ebd28b31b390adbc0205035e06
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-03 09:14:50 +0300

    LAZY-LOAD

--- components/InnerCategoryCard.vue
+++ components/InnerCategoryCard.vue
@@ -2,7 +2,20 @@
 <template>
   <div>
     <nuxt-link :to="`/category/${category.identifier}`">
-      <v-img v-if="category.image" :src="category.image"></v-img>
+      <v-img
+        v-if="category.image"
+        :src="category.image"
+        :lazy-src="category.image"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
       <p v-if="category.title">{{ category.title }}</p>
     </nuxt-link>
   </div>
--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <nuxt-link :to="`/product/${product.id}`">
-      <v-img :src="product.mainImageUrl" :alt="product.typeName"></v-img>
+      <v-img
+        :src="product.mainImageUrl"
+        :lazy-src="product.mainImageUrl"
+        :alt="product.typeName"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
       <h4>{{ product.name }}</h4>
       <p>{{ product.typeName }}</p>
       <p>{{ product.priceNumeral }}</p>
@@ -10,6 +23,8 @@
 </template>
 
 <script>
+// TODO собирать srcset scraper'ом
+// TODO заменить lazy-src на thumbnail этого изображения
 export default {
   name: 'ProductCard',
   props: {
