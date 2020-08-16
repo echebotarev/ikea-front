7b27e9972b48904d890eb70dacdada73aaaa8b00
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-16 17:13:08 +0300

    Cart: highlight

--- pages/Cart.vue
+++ pages/Cart.vue
@@ -46,13 +46,23 @@
                   "
                   :is-only-formatted="true"
                   symbol=".-"
+                  :class-name="
+                    product.price.price.mainPriceProps.hasHighlight &&
+                    product.qnt === 1
+                      ? 'highlight'
+                      : ''
+                  "
                 />
 
                 <Price
                   v-if="product.qnt > 1"
                   :price="product.price.price.mainPriceProps.price.integer"
                   symbol=".-"
-                  class="light"
+                  :class-name="`light ${
+                    product.price.price.mainPriceProps.hasHighlight
+                      ? 'highlight'
+                      : ''
+                  }`"
                 />
 
                 <div v-if="product.price.familyText" class="previous-price">
@@ -65,7 +75,7 @@
                     :is-only-formatted="true"
                     :prepend="`${product.price.price.previousPriceText}:`"
                     symbol=".-"
-                    class="light"
+                    class-name="light"
                   />
 
                   <Price
@@ -74,7 +84,7 @@
                       product.price.price.previousPriceProps.price.integer
                     "
                     symbol=".-"
-                    class="light"
+                    class-name="light"
                   />
                 </div>
               </div>
@@ -157,6 +167,14 @@ export default {
     font-weight: 700;
     color: #fff;
     padding: 0 0.5em;
+  }
+
+  .highlight {
+    display: inline-block;
+    background: #ffce00;
+    padding: 0.15rem 0.3rem;
+    box-shadow: 4px 4px 0 0 #ef4123;
+    margin: 0 0.375rem 0.375rem 0;
   }
 }
 </style>
