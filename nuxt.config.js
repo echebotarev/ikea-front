4c9c757e46bd2b0cc4990105fb504e9c42ef972a
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-17 21:38:07 +0300

    Loader: изменил цвет

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   loading: {
-    color: '#39b982',
+    color: '#0059aa',
   },
   /*
    ** Global CSS
