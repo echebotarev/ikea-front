4adf00dc6939ff69b12f7e702903b4ed6e79f930
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-20 18:05:03 +0300

    ApiService: используем конфиг для указания пути

--- services/ApiService.js
+++ services/ApiService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import config from '@/config'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:8080/api/v1`,
+  baseURL: config.apiUrl,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
