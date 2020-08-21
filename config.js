a9c187f0135d44f9627beb1475353f28343be453
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-21 10:15:58 +0300

    Api URL

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
 export default {
-  apiUrl: 'http://localhost:8080/api/v1',
+  get apiUrl() {
+    if (process.env.NODE_ENV === 'development') {
+      return 'http://localhost:8080/api/v1'
+    }
+
+    return process.server
+      ? 'http://localhost:8080/api/v1'
+      : 'http://ikea-dostavka.org'
+  },
 }
