18ed0dc50b38e95d14bbd29dd32244020b9c72cd
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-22 11:02:10 +0300

    Index: заголовки категорий

--- components/MainCategoryCard.vue
+++ components/MainCategoryCard.vue
@@ -3,16 +3,28 @@
   <div class="card-category">
     <nuxt-link :to="`/category/${category.identifier}`">
       <p v-if="category.title">{{ category.title }}</p>
-      <v-img v-if="category.title === null" :src="category.image"></v-img>
+      <div v-if="category.title === null">
+        <v-img :src="category.image"></v-img>
+        <h3 class="mb-4 mt-2">{{ CategoriesDict[category.identifier] }}</h3>
+      </div>
     </nuxt-link>
   </div>
 </template>
 <script>
+import CategoriesDict from '@/assets/data/categories'
+
 export default {
   name: 'MainCategoryCard',
   props: {
     category: { type: Object, default: () => {} },
   },
+  computed: {
+    CategoriesDict: () => CategoriesDict,
+  },
 }
 </script>
-<style scoped></style>
+<style scoped lang="scss">
+h3 {
+  color: #000;
+}
+</style>
