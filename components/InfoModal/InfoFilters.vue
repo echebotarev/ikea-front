d977e9958af9104289c1c2d6209e5cc5157ae2cd
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-26 10:53:04 +0300

    Filters: отображаем эл-ты сортировки

--- components/InfoModal/InfoFilters.vue
+++ components/InfoModal/InfoFilters.vue
@@ -2,7 +2,23 @@
   <v-row>
     <v-spacer></v-spacer>
     <v-col cols="10">
-      {{ data.sortOrders.name }}
+      <v-expansion-panels accordion flat>
+        <!-- Сортировка -->
+        <v-expansion-panel v-if="data.sortOrders.name">
+          <v-expansion-panel-header>
+            Сортировать
+          </v-expansion-panel-header>
+
+          <v-expansion-panel-content>
+            <v-row
+              v-for="value in prepareSortData(data.sortOrders.values)"
+              :key="value.id"
+            >
+              <v-col>{{ value.name }}</v-col>
+            </v-row>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-col>
     <v-spacer></v-spacer>
   </v-row>
@@ -15,6 +31,20 @@ export default {
     data: {
       type: Object,
       default: () => {},
+    },
+  },
+  methods: {
+    prepareSortData(data) {
+      const dictName = {
+        RELEVANCE: 'Популярность',
+        PRICE_LOW_TO_HIGH: 'Цена: от низкой до высокой',
+        PRICE_HIGH_TO_LOW: 'Цена: от высокой до низкой',
+        NAME_ASCENDING: 'По алфавиту: от А до Я',
+      }
+
+      return data
+        .filter((item) => item.id !== 'MOST_POPULAR')
+        .map((item) => Object.assign({}, item, { name: dictName[item.id] }))
     },
   },
 }
