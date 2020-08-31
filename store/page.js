38b551811f3f2f60ee7da29809262de592c9b634
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-31 17:27:05 +0300

    Breadcrumbs: обработка отсутствующей цепочки ссылок

--- store/page.js
+++ store/page.js
@@ -10,17 +10,18 @@ export const state = () => ({
 export const mutations = {
   SET_BREADCRUMBS(state, breadcrumbs) {
     const items = []
-    breadcrumbs.itemListElement.forEach((breadcrumb, index) => {
-      // TODO вычислять identifier в скрапере
-      const arr = breadcrumb.item.split('-')
-      const identifier = arr[arr.length - 1].replace('/', '')
+    breadcrumbs.itemListElement &&
+      breadcrumbs.itemListElement.forEach((breadcrumb, index) => {
+        // TODO вычислять identifier в скрапере
+        const arr = breadcrumb.item.split('-')
+        const identifier = arr[arr.length - 1].replace('/', '')
 
-      items.push({
-        text: breadcrumb.name,
-        href: identifier === 'products' ? '/' : `/category/${identifier}`,
-        disabled: breadcrumbs.itemListElement.length === index + 1,
+        items.push({
+          text: breadcrumb.name,
+          href: identifier === 'products' ? '/' : `/category/${identifier}`,
+          disabled: breadcrumbs.itemListElement.length === index + 1,
+        })
       })
-    })
     state.breadcrumbs = items
   },
 
