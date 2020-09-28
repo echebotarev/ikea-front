c4359c3786fb5773f33c524b9f0a98673042ca8b
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-09-28 09:49:40 +0300

    Info: Мой номер телефона

--- assets/data/links.js
+++ assets/data/links.js
@@ -1,5 +1,5 @@
 export default [
   { label: 'Все товары', url: '/' },
-  { label: 'Информация', url: '/information' },
+  { label: 'Контакты', url: '/information' },
   { label: 'Корзина', url: '/cart' },
 ]
