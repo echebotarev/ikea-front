9f752a6d8856a9eab248f3200f229cc0eae69e82
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-10-20 17:33:09 +0300

    Повысил курс тенге

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const percent = 30
 export const assemblyPercent = 5
-export const KZT = 5.65
+export const KZT = 6
 export const rubSymbol = '₽'
 export const kztSymbol = '₸'
 export const currencySymbol = '₸'
