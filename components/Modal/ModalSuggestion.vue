cc952dfdda93974b0105c9047e8bfd50dfd08482
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-10-27 18:16:27 +0300

    Suggestion: перейти в корзину

--- components/Modal/ModalSuggestion.vue
+++ components/Modal/ModalSuggestion.vue
@@ -4,50 +4,63 @@
     <v-col cols="10">
       <h2>{{ data.title }}</h2>
 
-      <div
-        v-for="(product, index) in data.suggestion"
-        :key="product.identifier"
-      >
-        <v-row>
-          <v-col cols="10">
-            <nuxt-link :to="`/product/${product.identifier}`">
-              <v-row>
-                <v-col cols="4">
-                  <v-img
-                    :src="getImage(imgPreview(product).content.url, 2)"
-                  ></v-img>
-                </v-col>
+      <div class="suggestion-content">
+        <div
+          v-for="(product, index) in data.suggestion"
+          :key="product.identifier"
+        >
+          <v-row>
+            <v-col cols="10">
+              <nuxt-link :to="`/product/${product.identifier}`">
+                <v-row>
+                  <v-col cols="4">
+                    <v-img
+                      :src="getImage(imgPreview(product).content.url, 2)"
+                    ></v-img>
+                  </v-col>
 
-                <v-col cols="8">
-                  <h4>{{ product.name }}</h4>
-                  <div class="product-description mb-5">
-                    {{ product.price.productDescription }}
-                  </div>
-                  <Price
-                    :price="product.price.price.mainPriceProps.price.integer"
-                  />
-                </v-col>
-              </v-row>
-            </nuxt-link>
-          </v-col>
+                  <v-col cols="8">
+                    <h4>{{ product.name }}</h4>
+                    <div class="product-description mb-5">
+                      {{ product.price.productDescription }}
+                    </div>
+                    <Price
+                      :price="product.price.price.mainPriceProps.price.integer"
+                    />
+                  </v-col>
+                </v-row>
+              </nuxt-link>
+            </v-col>
 
-          <v-col cols="2">
-            <v-btn
-              elevation="0"
-              fab
-              color="#0058a3"
-              width="40"
-              height="40"
-              @click="add({ product, qnt: 1 })"
-            >
-              <v-icon color="#fff">
-                mdi-basket-plus-outline
-              </v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-divider v-if="index !== data.suggestion.length - 1"></v-divider>
+            <v-col cols="2">
+              <v-btn
+                elevation="0"
+                fab
+                color="#0058a3"
+                width="40"
+                height="40"
+                @click="add({ product, qnt: 1 })"
+              >
+                <v-icon color="#fff">
+                  mdi-basket-plus-outline
+                </v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-divider v-if="index !== data.suggestion.length - 1"></v-divider>
+        </div>
       </div>
+
+      <v-btn
+        to="/cart"
+        block
+        rounded
+        color="#0058a3"
+        min-height="50"
+        class="button mt-10"
+      >
+        Перейти в корзину
+      </v-btn>
     </v-col>
     <v-spacer></v-spacer>
   </v-row>
@@ -86,5 +99,9 @@ export default {
 .product-description {
   font-size: 0.875rem;
   color: #484848;
+}
+.suggestion-content {
+  height: 60%;
+  overflow: scroll;
 }
 </style>
