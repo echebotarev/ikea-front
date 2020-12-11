0b4478cac0db21298ae445d1cb42c0c97197e3e6
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-12-11 08:19:33 +0300

    Bug: если в начале или в конце почты указан пробел, то валидатор выдает ошибку

--- pages/Cart.vue
+++ pages/Cart.vue
@@ -491,7 +491,7 @@ export default {
 
     checkEmail(value) {
       const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      return pattern.test(value)
+      return pattern.test(value.trim())
     },
 
     getLetterProducts() {
