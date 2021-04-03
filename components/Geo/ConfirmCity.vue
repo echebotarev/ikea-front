33f6c97d4c6e53eec2717bb12c871f54ee86e714
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-04-03 14:45:39 +0300

    GEO: блок с вопросом о городе

--- components/Geo/ConfirmCity.vue
+++ components/Geo/ConfirmCity.vue
@@ -1,6 +1,10 @@
 <template>
   <v-card class="confirm-city">
     <v-card-title>{{ geo && geo.city }} - это ваш город?</v-card-title>
+    <v-card-actions class="confirm-city-actions float-right">
+      <v-btn elevation="0" width="100px" class="button button-black">Да</v-btn>
+      <v-btn elevation="0" width="100px" class="button button-black">Нет</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
