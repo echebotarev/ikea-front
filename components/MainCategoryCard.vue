5d230042a38629887894eaf006ccf852dc1868cd
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-04-14 09:31:07 +0300

    Main: кликабельные изображения

--- components/MainCategoryCard.vue
+++ components/MainCategoryCard.vue
@@ -2,18 +2,20 @@
 <template>
   <div class="card-category">
     <div v-if="category.image">
-      <v-img
-        :src="category.image"
-        lazy-src="/images/placeholder-horizontal.png"
-        aspect-ratio="1.8"
-      >
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-icon x-large>mdi-image-multiple-outline</v-icon>
-          </v-row>
-        </template>
-      </v-img>
-      <h3 class="mb-4 mt-2">{{ CategoriesDict[category.identifier] }}</h3>
+      <nuxt-link :to="`/category/${category.identifier}`">
+        <v-img
+          :src="category.image"
+          lazy-src="/images/placeholder-horizontal.png"
+          aspect-ratio="1.8"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-icon x-large>mdi-image-multiple-outline</v-icon>
+            </v-row>
+          </template>
+        </v-img>
+        <h3 class="mb-4 mt-2">{{ CategoriesDict[category.identifier] }}</h3>
+      </nuxt-link>
     </div>
     <nuxt-link v-else :to="`/category/${category.identifier}`">
       <p v-if="category.title">{{ category.title }}</p>
