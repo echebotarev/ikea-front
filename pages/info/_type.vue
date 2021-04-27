3d17fb24572b72b1d6a0056e03bdcad3af2707da
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-04-27 19:29:51 +0300

    Info: страница с описанием процесса после заказа

---
+++ pages/info/_type.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1 class="text-center mb-10">{{ page.title }}</h1>
+    <v-row>
+      <v-col>
+        <v-card color="#0058a3" class="py-5 px-15">
+          <div class="text-center">
+            <v-icon x-large color="#fff">mdi-numeric-1-circle</v-icon>
+          </div>
+          <v-card-title class="card-title text-center text-break">
+            Заказ отправлен на указанную вами почту
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card color="#0058a3" class="py-5 px-15">
+          <div class="text-center">
+            <v-icon x-large color="#fff">mdi-numeric-2-circle</v-icon>
+          </div>
+          <v-card-title class="card-title text-center text-break">
+            В ближайшее время мы с вами свяжемся
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Information',
+
+  data() {
+    return {
+      page: {},
+    }
+  },
+
+  beforeMount() {
+    const { type } = this.$route.params
+    switch (type) {
+      case 'order-is-sent':
+        return (this.page = {
+          type,
+          title: 'Заказ отправлен',
+        })
+    }
+  },
+
+  methods: {},
+}
+</script>
+
+<style scoped lang="scss">
+.card-title {
+  color: #fff;
+  display: block;
+}
+</style>
