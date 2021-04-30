4644684e8e36df7340d324da165e8e95683b6407
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-04-30 15:03:52 +0300

    Metrika: попытка уменбшить ее влияние

--- plugins/ya.js
+++ plugins/ya.js
@@ -4,40 +4,42 @@ export default ({ app, $getCookie }, inject) => {
     return
   }
 
-  ;(function (m, e, t, r, i, k, a) {
-    m[i] =
-      m[i] ||
-      function () {
-        ;(m[i].a = m[i].a || []).push(arguments)
-      }
-    m[i].l = 1 * new Date()
-    // eslint-disable-next-line no-unused-expressions,no-sequences
-    ;(k = e.createElement(t)),
-      (a = e.getElementsByTagName(t)[0]),
-      (k.async = 1),
-      (k.src = r),
-      a.parentNode.insertBefore(k, a)
-  })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
-
-  // eslint-disable-next-line no-undef
-  inject('metrika', ym)
-
-  const cookieId = $getCookie('cookieId')
-  // eslint-disable-next-line no-undef
-  ym('67230112', 'init', {
-    webvisor: true,
-    trackHash: true,
-    userParams: cookieId
-      ? {
-          UserID: cookieId,
+  window.onNuxtReady(() => {
+    ;(function (m, e, t, r, i, k, a) {
+      m[i] =
+        m[i] ||
+        function () {
+          ;(m[i].a = m[i].a || []).push(arguments)
         }
-      : {},
-  })
+      m[i].l = 1 * new Date()
+      // eslint-disable-next-line no-unused-expressions,no-sequences
+      ;(k = e.createElement(t)),
+        (a = e.getElementsByTagName(t)[0]),
+        (k.async = 1),
+        (k.src = r),
+        a.parentNode.insertBefore(k, a)
+    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
 
-  app.router.afterEach((to, from) => {
     // eslint-disable-next-line no-undef
-    ym('67230112', 'hit', `${document.location.origin}${to.fullPath}`)
+    inject('metrika', ym)
+
+    const cookieId = $getCookie('cookieId')
     // eslint-disable-next-line no-undef
-    cookieId && ym('67230112', 'setUserID', cookieId)
+    ym('67230112', 'init', {
+      webvisor: true,
+      trackHash: true,
+      userParams: cookieId
+        ? {
+            UserID: cookieId,
+          }
+        : {},
+    })
+
+    app.router.afterEach((to, from) => {
+      // eslint-disable-next-line no-undef
+      ym('67230112', 'hit', `${document.location.origin}${to.fullPath}`)
+      // eslint-disable-next-line no-undef
+      cookieId && ym('67230112', 'setUserID', cookieId)
+    })
   })
 }
