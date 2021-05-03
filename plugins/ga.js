4bbf7f6e18c7345ff264167963bbc3731aad3f9e
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-05-03 13:15:29 +0300

    Lighthouse: откатил изменение ga

--- plugins/ga.js
+++ plugins/ga.js
@@ -2,26 +2,22 @@ export default ({ app }, inject) => {
   /*
    ** Будет работать только на стороне клиента и только когда режим разработки будет "production"
    */
-  // if (process.env.NODE_ENV !== 'production') {
-  //   inject('gtag', function () {})
-  //   return
-  // }
+  if (process.env.NODE_ENV !== 'production') {
+    inject('gtag', function () {})
+    return
+  }
 
-  window.onNuxtReady(() => {
-    ;(function (w, d, s, l, i) {
-      setTimeout(() => {
-        w[l] = w[l] || []
-        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
-        const f = d.getElementsByTagName(s)[0]
-        const j = d.createElement(s)
-        // eslint-disable-next-line eqeqeq
-        const dl = l != 'dataLayer' ? '&l=' + l : ''
-        j.async = true
-        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
-        f.parentNode.insertBefore(j, f)
-      }, 2000)
-    })(window, document, 'script', 'dataLayer', 'GTM-P2K8RLJ')
-  })
+  ;(function (w, d, s, l, i) {
+    w[l] = w[l] || []
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+    const f = d.getElementsByTagName(s)[0]
+    const j = d.createElement(s)
+    // eslint-disable-next-line eqeqeq
+    const dl = l != 'dataLayer' ? '&l=' + l : ''
+    j.async = true
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+    f.parentNode.insertBefore(j, f)
+  })(window, document, 'script', 'dataLayer', 'GTM-P2K8RLJ')
 
   window.dataLayer = window.dataLayer || []
   function gtag() {
