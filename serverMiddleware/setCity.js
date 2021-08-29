0818e45bf17910fed2aa3e604e792d2c49b61cc5
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-08-29 10:30:57 +0300

    301 redirect

--- serverMiddleware/setCity.js
+++ serverMiddleware/setCity.js
@@ -1,10 +1,21 @@
 import domains from './../constants/domains.js'
 
-export default function ({ headers }, res, next) {
+export default function (req, res, next) {
+  const { headers } = req
+
   const city = headers.host.split('.')[0]
+
   // если есть домен 3го уровня, то выставляем имя города в env
   // если нет, значит выставляем Актау
-  process.env.city = domains[city] ? city : 'aktau'
-
-  next()
+  if (domains[city]) {
+    process.env.city = city
+    next()
+  } else {
+    res.writeHead(301, {
+      Location: `${
+        process.env.NODE_ENV === 'production' ? 'https' : 'http'
+      }://aktau.${headers.host}${req.url}`,
+    })
+    res.end()
+  }
 }
