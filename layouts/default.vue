1ae5246a8505fb7d00cf9050cf669141199d8bdd
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-10-15 12:09:09 +0300

    Отключение Уральска

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,13 @@
     <v-main>
       <Message v-if="shopId === '001'" />
 
+      <AttentionButton
+        v-if="shopId === '003'"
+        :has-button="false"
+        class-name="sticky"
+        :text="`В данный момент мы не принимаем заказы в городе Уральск.<br />Приносим свои извинения<br />Команда doma-doma.org`"
+      />
+
       <Header />
 
       <client-only v-if="$vuetify.breakpoint.smAndDown">
@@ -54,6 +61,7 @@ export default {
     Footer: hydrateWhenVisible(() => import('@/components/Footer.vue')),
     ChooseCity: hydrateWhenVisible(() => import('@/components/Geo/ChooseCity')),
     NavLinks: hydrateWhenVisible(() => import('@/components/NavLinks')),
+    AttentionButton: () => import('@/components/AttentionButton'),
   },
   middleware: 'fetchOrders',
   computed: {
