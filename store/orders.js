68a11b5141af56615d980e4f56ecb3962a27aae8
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-11-28 18:28:15 +0300

    Events: category

--- store/orders.js
+++ store/orders.js
@@ -59,7 +59,7 @@ export const actions = {
     // this.$metrika(67230112, 'reachGoal', 'addToCart')
 
     const { product, qnt } = payload
-    this.$gtag('event', 'addToCart')
+    this.$gtag('event', 'addToCart', { event_category: 'events' })
     this.$gtag.ec({
       ecommerce: {
         currencyCode: 'RUB',
@@ -135,7 +135,7 @@ export const actions = {
     const shopId = rootState.geo.shopId
     const coefficient = rootGetters['variables/coefficient']
 
-    this.$gtag('event', 'purchase')
+    this.$gtag('event', 'purchase', { event_category: 'events' })
     // eslint-disable-next-line no-unreachable
     this.$gtag.ec({
       ecommerce: {
